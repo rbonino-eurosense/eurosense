ad6d9c9d6b7a5c644731d18f82938f4b7a5aeaf8
rbonino-eurosense <roberto.bonino@volteuropa.org>
2026-09-09 18:33:02 +0100

    message2

--- StoryBoard2.py
+++ StoryBoard2.py
@@ -18,7 +18,7 @@ import os
 IDselect = 1  #set to 1 if you want to select specific metaID
 metaID = "MVE"
 
-print ( os.listdir("../R Eurosense Data/"))
+print ( os.listdir("Data"))
 file_path = "../R Eurosense Data/2026.06.08/CSVExport-2026.06.08_translated.csv"
 ##file_path = "./cca+climate/cca+climate.csv"
 
